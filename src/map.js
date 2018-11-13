843a3386468138a762698dc78d10797ad47a417a
Iiro Valkoja <iiro.valkoja@courses.tamk.fi>
2018-11-14 00:33:59 +0200

    Refactoring, started map class

---
+++ src/map.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.addEventListener('load', () => { const map = new Map(); });
+
+class Map
+{
+    constructor()
+    {
+        document.body.innerHTML = 'Hello world v2';
+    }
+}
